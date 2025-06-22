d097dd5a07d188df3c55595a8155b13cf61479f7
Benjamin Herrera <BenHerrera1044@outlook.com>
2025-06-22 01:15:38 -0600

    Updated to reflect personal details

--- src/frontpage.jsx
+++ src/frontpage.jsx
@@ -6,17 +6,17 @@ export default function Frontpage() {
   useEffect(() => {
     const video = document.getElementById("bg-video");
     if (video) {
-      video.playbackRate = 1.0;
+      video.playbackRate = 0.75;
     }
   }, []);
 
   // **** RENDER ****
   return (
-    <div className="relative h-screen w-full overflow-hidden bg-slate-600 text-center">
+    <div className="relative h-screen w-full overflow-hidden bg-slate-700 text-center">
       {/* Background video */}
       <video
         className="absolute top-0 left-0 z-0 h-full w-full object-cover blur-sm"
-        src="/bg-video_1.mp4"
+        src="/bg-video_a.mp4"
         autoPlay
         loop
         muted
@@ -24,43 +24,14 @@ export default function Frontpage() {
         controls={false}
         id="bg-video"
       />
-      <div className="absolute top-0 left-0 w-full h-full bg-white/20"></div>
+      <div className="absolute top-0 left-0 w-full h-full bg-black/25"></div>
 
       {/* Front Content */}
-      <div className="relative z-10 flex h-screen flex-col items-center justify-center space-y-15">
-        <p className="roboto-bold-italic tracking-[-0.1em] text-[128px] md:text-[256px] leading-none">EZRA</p>
-        <p className="merriweather-normal text-[48px] md:text-[64px] leading-none">
-          Launching
-          <br />
-          Soon!
+      <div className="relative z-10 flex h-screen flex-col items-center justify-center space-y-15 text-white">
+        <p className="libre-franklin-bold text-bold text-[128px] md:text-[164px] leading-none">BENJAMIN HERRERA</p>
+        <p className="libre-franklin-normal text-[48px] md:text-[64px] leading-none">
+          Website Launching Soon!
         </p>
-
-        {/* Contact Info */}
-        <div className="flex flex-col items-center space-y-2">
-          <p className="inter-normal text-[28px] md:text-[32px] px-4">
-            Connecting Believers Under One Platform
-          </p>
-          <div className="inter-normal rounded-lg text-[24px] mt-4">
-            Contact Us!
-          </div>
-          <p className="inter-bold text-[18px]">info@theezraco.com</p>
-
-          {/* Socials */}
-          <div className="flex space-x-4">
-            <div className="text-[32px]">
-              <FaSquareFacebook />
-            </div>
-            <div className="text-[32px]">
-              <FaInstagram />
-            </div>
-            <div className="text-[32px]">
-              <FaXTwitter />
-            </div>
-            <div className="text-[32px]">
-              <FaLinkedin />
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
