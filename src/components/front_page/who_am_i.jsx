9fe4f50769995bea7d63ede1712cf4ef17c19eba
Benjamin Herrera <BenHerrera1044@outlook.com>
2025-10-08 14:39:05 -0700

    Applied some finishing touches to the who am i page

--- src/components/front_page/who_am_i.jsx
+++ src/components/front_page/who_am_i.jsx
@@ -11,23 +11,26 @@ export default function WhoAmI() {
   return (
     <div className="relative h-screen w-full overflow-hidden bg-slate-700 text-center">
       {/* Front Content */}
-      <div className="relative z-10 flex h-full justify-center">
+      <div className="relative z-10 flex h-full items-center justify-center">
         {/* Image + Text Container */}
-        <div className="flex flex-col items-center text-white md:space-x-12 xl:flex-row">
+        <div className="flex flex-col items-center px-[32px] text-white md:space-x-12 lg:flex-row">
           {/* Image */}
-          <div className="text-[256px] drop-shadow-2xl">
+          <div className="text-[256px] drop-shadow-2xl lg:text-[320px] xl:text-[344px]">
             🛰️
           </div>
 
           {/* Text */}
-          <div className="px-[16px] mb-[3vh] flex flex-col justify-center space-y-6 lg:mr-[6vh] xl:text-right">
-            <p className="mb-[-0.5vh] text-[64px] md:text-[72px] lg:text-[80px] xl:text-[128px]">
-            </p>
-            <p className="libre-franklin-bold text-[48px] leading-none md:text-[56px] lg:text-[64px] xl:text-[80px]">
+          <div className="mx-[25px] flex flex-col justify-center space-y-6 lg:text-left">
+            <p className="libre-franklin-bold text-[48px] leading-none md:mt-[10vh] md:text-[56px] lg:text-[64px] xl:text-[80px]">
               Who Am I?
             </p>
-            <p className="libre-franklin-normal text-[16px] leading-[1.3] md:text-[24px] lg:text-[32px] xl:pb-[5vh] xl:text-[40px]">
-              I am a Master's Student studying <i>Computer Science</i> at <u>Arizona State University</u>. My research work is in <b>Fault Tolerance for ML Models</b>, and <b>Model Compression & Optimization</b>. This ties to applications in <i>Reat-time Geospatial Analysis</i> and <i>ML for Space Systems.</i>
+            <p className="mb-[3vh] libre-franklin-normal text-[16px] leading-[1.3] md:mt-[2vh] md:text-[24px] lg:text-[32px] xl:text-[40px]">
+              I am a Master's Student studying <i>Computer Science</i> at{" "}
+              <u>Arizona State University</u>. My research work is in{" "}
+              <b>Fault Tolerance for ML Models</b>, and{" "}
+              <b>Model Compression & Optimization</b>. This ties to applications
+              in <i>Real-time Geospatial Analysis</i> and{" "}
+              <i>ML for Space Systems.</i>
             </p>
           </div>
         </div>
