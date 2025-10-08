c088ceb2a0ad7b8d4f05b3c0a8a9bcc8ce6704fa
Benjamin Herrera <BenHerrera1044@outlook.com>
2025-10-08 14:11:27 -0700

    Finishing touches to greeting page

--- src/components/front_page/greeting.jsx
+++ src/components/front_page/greeting.jsx
@@ -26,14 +26,14 @@ export default function Greeting() {
       {/* Front Content */}
       <div className="relative z-10 flex h-full items-end justify-center">
         {/* Image + Text Container */}
-        <div className="flex flex-col lg:flex-row items-center text-white md:space-x-12">
+        <div className="flex flex-col xl:flex-row items-center text-white md:space-x-12">
           {/* Text */}
-          <div className="mb-[3vh] flex flex-col justify-center space-y-6 lg:text-right lg:mr-[6vh]">
-            <p className="text-[64px] mb-[-0.5vh] md:text-[96px] lg:text-[128px]">👋</p>
-            <p className="libre-franklin-bold text-[48px] leading-none md:text-[64px] lg:text-[80px]">
+          <div className="mb-[3vh] flex flex-col justify-center space-y-6 xl:text-right lg:mr-[6vh]">
+            <p className="text-[64px] mb-[-0.5vh] md:text-[72px] lg:text-[80px] xl:text-[128px]">👋</p>
+            <p className="libre-franklin-bold text-[48px] leading-none md:text-[56px] lg:text-[64px] xl:text-[80px]">
               Hello there!
             </p>
-            <p className="libre-franklin-normal text-[16px] leading-none md:text-[24px] lg:text-[32px] lg:pb-[5vh]">
+            <p className="libre-franklin-normal text-[16px] leading-none md:text-[24px] lg:text-[32px] xl:text-[40px] xl:pb-[5vh]">
               Ako si <b>Benjamin Herrera</b>
             </p>
           </div>
@@ -42,7 +42,7 @@ export default function Greeting() {
           <img
             src="/self_photo.png"
             alt="Benjamin Herrera"
-            className="h-[60vh] w-[60vh] object-cover"
+            className="h-[45vh] w-[45vh] xl:h-[60vh] xl:w-[60vh] object-cover"
           />
         </div>
       </div>
